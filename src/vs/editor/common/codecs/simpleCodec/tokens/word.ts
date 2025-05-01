f85dcb6c83cf187b85feba949aad1e76d4783862
Oleg Solomko <legomushroom@gmail.com>
2025-05-01 15:11:55 -0700

    support 'void' front matter record values

--- src/vs/editor/common/codecs/simpleCodec/tokens/word.ts
+++ src/vs/editor/common/codecs/simpleCodec/tokens/word.ts
@@ -6,14 +6,13 @@
 import { BaseToken } from '../../baseToken.js';
 import { Line } from '../../linesCodec/tokens/line.js';
 import { Range } from '../../../../../editor/common/core/range.js';
-import { Position } from '../../../../../editor/common/core/position.js';
 
 /**
  * A token that represent a word - a set of continuous
  * characters without stop characters, like a `space`,
  * a `tab`, or a `new line`.
  */
-export class Word extends BaseToken {
+export class Word<TValue extends string = string> extends BaseToken {
 	constructor(
 		/**
 		 * The word range.
@@ -23,7 +22,7 @@ export class Word extends BaseToken {
 		/**
 		 * The string value of the word.
 		 */
-		public readonly text: string,
+		public readonly text: TValue,
 	) {
 		super(range);
 	}
@@ -34,16 +33,20 @@ export class Word extends BaseToken {
 	 */
 	public static newOnLine(
 		text: string,
-		line: Line,
+		line: Line | number,
 		atColumnNumber: number,
 	): Word {
-		const { range } = line;
+		const startLineNumber = (typeof line === 'number')
+			? line
+			: line.range.startLineNumber;
 
-		const startPosition = new Position(range.startLineNumber, atColumnNumber);
-		const endPosition = new Position(range.startLineNumber, atColumnNumber + text.length);
+		const range = new Range(
+			startLineNumber, atColumnNumber,
+			startLineNumber, atColumnNumber + text.length
+		);
 
 		return new Word(
-			Range.fromPositions(startPosition, endPosition),
+			range,
 			text,
 		);
 	}
